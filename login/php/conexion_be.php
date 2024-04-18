a818fb961fc421cf59eadeda26e3bdb7e04fa86b
jonaca43 <135735994+jonaca43@users.noreply.github.com>
2024-04-18 14:32:25 -0500

    Add files via upload

---
+++ login/php/conexion_be.php
@@ -0,0 +1,11 @@
+<?php
+ 
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+   
+   /* if($conexion){
+        echo'conexion exitosa';
+    }else{
+        echo 'no se conecto ';
+    }
+*/
+?>
